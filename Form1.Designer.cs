a7ed1e524e90d8d868595048f1ef689af7378897
mphofiloe <92067886+mphofiloe@users.noreply.github.com>
2021-11-30 04:54:18 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace the_probability
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PERM = new System.Windows.Forms.Label();
+            this.COMB = new System.Windows.Forms.Label();
+            this.PERMU = new System.Windows.Forms.Label();
+            this.COMBI = new System.Windows.Forms.Label();
+            this.PER = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(449, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CALCULATE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(449, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(449, 265);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "EXIT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "OCCURENCE A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "OCCURENCE B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "OCCURENCE C";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(202, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(202, 203);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(202, 265);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(199, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "THE PROBABILITY";
+            // 
+            // PERM
+            // 
+            this.PERM.AutoSize = true;
+            this.PERM.Location = new System.Drawing.Point(558, 187);
+            this.PERM.Name = "PERM";
+            this.PERM.Size = new System.Drawing.Size(0, 13);
+            this.PERM.TabIndex = 19;
+            // 
+            // COMB
+            // 
+            this.COMB.AutoSize = true;
+            this.COMB.Location = new System.Drawing.Point(583, 228);
+            this.COMB.Name = "COMB";
+            this.COMB.Size = new System.Drawing.Size(0, 13);
+            this.COMB.TabIndex = 20;
+            // 
+            // PERMU
+            // 
+            this.PERMU.AutoSize = true;
+            this.PERMU.Location = new System.Drawing.Point(583, 183);
+            this.PERMU.Name = "PERMU";
+            this.PERMU.Size = new System.Drawing.Size(0, 13);
+            this.PERMU.TabIndex = 21;
+            this.PERMU.Click += new System.EventHandler(this.PERMU_Click);
+            // 
+            // COMBI
+            // 
+            this.COMBI.AutoSize = true;
+            this.COMBI.Location = new System.Drawing.Point(523, 238);
+            this.COMBI.Name = "COMBI";
+            this.COMBI.Size = new System.Drawing.Size(0, 13);
+            this.COMBI.TabIndex = 22;
+            // 
+            // PER
+            // 
+            this.PER.AutoSize = true;
+            this.PER.Location = new System.Drawing.Point(523, 187);
+            this.PER.Name = "PER";
+            this.PER.Size = new System.Drawing.Size(0, 13);
+            this.PER.TabIndex = 23;
+            this.PER.Click += new System.EventHandler(this.JJ_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 450);
+            this.Controls.Add(this.PER);
+            this.Controls.Add(this.COMBI);
+            this.Controls.Add(this.PERMU);
+            this.Controls.Add(this.COMB);
+            this.Controls.Add(this.PERM);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label PERM;
+        private System.Windows.Forms.Label COMB;
+        private System.Windows.Forms.Label PERMU;
+        private System.Windows.Forms.Label COMBI;
+        private System.Windows.Forms.Label PER;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace the_probability
+{
+    public partial class Form1 : Form
+    {
+        float A;
+        float B;
+        float C;
+        float D;
+        float E;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            A = float.Parse(textBox1.Text);
+            B = float.Parse(textBox2.Text);
+            C = float.Parse(textBox3.Text);
+            //VALUES ARE TAKEN ABOVE...
+            textBox1.Text = (A / (A + B + C)).ToString();
+            //FORMULA FOR PROBABILITY
+            textBox2.Text = (B / (A + B + C)).ToString();
+            textBox3.Text = (C / (A + B + C)).ToString();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            //thos will clear the probability holding fields
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+
+            //input holding fields will be cleared by this
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            int k, m;
+            double perm, comb;
+            k = Convert.ToInt32(textBox4.Text);
+            m = Convert.ToInt32(textBox5.Text);
+
+            perm = fac(k) / fac(k - m);
+
+            comb = fac(k) / (fac(k - m) * fac(k));
+
+            PER.Text = Convert.ToString(perm);
+            COMBI.Text = Convert.ToString(comb);
+
+
+        }
+        int fac(int a)
+        {
+            int i, fa = 1;
+            for (i = 0; i <= a; i++)
+            { fa *= i; }
+            return fa;
+        } 
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PERMU_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void JJ_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
